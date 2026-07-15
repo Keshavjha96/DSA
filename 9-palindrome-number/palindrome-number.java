e9ba2fd17cf7e5e477717fd69088cf967112ec96
Keshavjha96 <keshav.jha9625@gmail.com>
2026-07-16 00:05:10 +0530

    Time: 4 ms (100.00%) | Memory: 45.9 MB (53.65%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+          int n=x;
+          if(x<0){return false;}
+        while(x!=0){
+            int lastdigit=x%10;
+            x=x/10;
+            rev=(rev*10)+lastdigit;
+        }
+        if(rev==n){
+            return true;
+        }
+        else return false;
+    }
+}
